f106ae13009dfeb221a86650193ab96b43e4e975
lucassantosrc7 <lucassantosrc7@gmail.com>
2025-11-02 23:47:51 -0300

    02.11 23:47

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3618474519"
+    serviceWorkerVersion: "1960985541"
   }
 });
