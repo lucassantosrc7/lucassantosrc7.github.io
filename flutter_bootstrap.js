48e71fea3f863ac73d96077a1027e169e6a4b47f
lucassantosrc7 <lucassantosrc7@gmail.com>
2026-05-07 10:47:50 -0300

    Deploy DEV - 2026-05-07 10:47:50

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1741552850"
+    serviceWorkerVersion: "2211115787"
   }
 });
